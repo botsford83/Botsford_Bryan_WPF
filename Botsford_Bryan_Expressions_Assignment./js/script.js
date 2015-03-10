033592e33b771ee9e34d2e405ef4975ea93d3a1e
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-09 21:26:42 -0500

    Prompts
    I will be adding prompts to my script.js file asking for information
    about the right angled triangle.

--- Botsford_Bryan_Expressions_Assignment./js/script.js
+++ Botsford_Bryan_Expressions_Assignment./js/script.js
@@ -10,7 +10,10 @@ var rightTriangle;
 var A;
 var B;
 
-
+//Prompts
+rightTriangle = confirm("The Pythagorean Theorem is used to find the hypotenuse of a right angled triangle.  The formula is a2 + b2 = c2.");
+A = Number(prompt("What is the value of side A"));
+B = Number(prompt("What is the value of side B"));
 
 
 
