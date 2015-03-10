c73f9d6d0896f906a6ed513a391c229589c0b301
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-10 12:45:57 -0500

    variables
    I will be adding three different types of variables including an array,
    a number, and a string.

--- Botsford_Bryan_Expressions_Assignment./js/script.js
+++ Botsford_Bryan_Expressions_Assignment./js/script.js
@@ -1,3 +1,8 @@
 /* Created by Bryan Botsford on 3/9/2015
  Bryan W. Botsford
  SDI 1503 */
+
+//Variables
+var assignmentNames = ["quiz", "reading", "test", "research"];
+var numberOfAssignments = 4;
+var total = "400 total points";
