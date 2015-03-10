75739f5b5ab6bc6fa2a522ae62bd7d128af9acb4
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-10 12:47:29 -0500

    outputs
    I will be creating outputs to figure out the total amount of points out
    of 400 the user scored and also the final average grade.

--- Botsford_Bryan_Expressions_Assignment./js/script.js
+++ Botsford_Bryan_Expressions_Assignment./js/script.js
@@ -12,3 +12,7 @@ quiz = Number(prompt("What was your quiz grade?"));
 reading = Number(prompt("What was your reading grade?"));
 test = Number(prompt("What was your test grade?"));
 research = Number(prompt("What was your research grade?"));
+
+//Outputs
+console.log("The following is the the points you earned on all of the assignments" + " " + (quiz + reading + test + research) + "/" + total);
+console.log("Your Final Grade is" + " " + (quiz + reading + test + research) / numberOfAssignments);
