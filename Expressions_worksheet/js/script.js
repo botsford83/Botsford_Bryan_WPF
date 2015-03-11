2cfc24da412ce26236dc582fc3f9ab7a62faebc5
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-11 06:59:48 -0500

    Problem 1
    I will be solving problem one

---
+++ Expressions_worksheet/js/script.js
@@ -0,0 +1,14 @@
+/**
+ * Created by bryanbotsford1 on 3/11/15.
+ */
+/* Dogs age 7 times faster than humans so a dog that is 1 year old
+in human years is 7 years old in "dog years."  Calculate how old
+Sparky the pitbull is in dog years based on his actual age
+ */
+
+var sparkysAge;
+
+
+sparkysAge = Number(prompt("How old is sparky in human years"));
+
+console.log("Sparky is" + " " + sparkysAge + " " + "human years old which is" + " " + sparkysAge * 7 + "in dog years" + ".");
