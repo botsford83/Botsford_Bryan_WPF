b17d2324ee76bad7e74d0267a369f5ba934966f2
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-16 18:20:41 -0500

    Staring project
    I will be starting the conditional assignment and adding my page.html
    file along with my script.js file.  I will also start to add prompts
    and my variables to my script.js file.

---
+++ Botsford_Bryan_Conditionals_Assignment./js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Created by bryanbotsford1 on 3/15/15.
+ */
+/* Bryan_Botsford
+   March_2015
+   SDI_Conditionals_Assignment
+ */
+
+// Alerts
+
+
+// Prompts
+A = Number(prompt("Please enter the value of side A, if no value is given, press OK"));
+B = Number(prompt("Please enter the value of side B, if no value is given, press OK"));
+C = Number(prompt("Please enter the value of side C, if no value is given, press OK"));
+
+// Variables
+var sideA = A * A;
+var sideB = B * B;
+var sideC = C * C;
+
+
+// Console
