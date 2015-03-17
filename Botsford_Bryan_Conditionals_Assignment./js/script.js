8833631300f3309b57f43f57e3e61f15b5c13b94
Bryan Botsford <bwbotsford@fullsail.edu>
2015-03-16 20:40:01 -0500

    creating outputs
    I will be creating outputs, touching up the code for any errors, and I
    will be adding notes on how the code works.

--- Botsford_Bryan_Conditionals_Assignment./js/script.js
+++ Botsford_Bryan_Conditionals_Assignment./js/script.js
@@ -6,31 +6,49 @@
    SDI_Conditionals_Assignment
  */
 
-// Alerts
-
+// alerts
+// code to alert user they will be using pythagoras theorem
+alert("This will allow you to use Pythagoras Theorem (A² + B² = C²) to find the hypotenuse of a right triangle.");
 
 // Prompts
-A = Number(prompt("Please enter the value of side A, if no value is given, press OK"));
+// code to prompt user for the value of each side and cast it into a number
+A = Number(prompt("Please enter the value of side A, if no value is given, Press OK"));
 B = Number(prompt("Please enter the value of side B, if no value is given, press OK"));
 C = Number(prompt("Please enter the value of side C, if no value is given, press OK"));
+// code to check if the user is trying to find the value of side C
+if (C == " "){
+    alert("You are trying to find the value for side C");
+// code to check if the user is trying to find the value of side B
+} else if (B == ""){
+    alert("You are trying to find the value of side B");
+// code to check if the user is trying to find the value of side A
+} else {
+    alert("You are trying to find the value of side A");// alerts user they are trying to find side A
+}
 
 // Variables
+// created variable to square the users input from the prompts
 var sideA = A * A;
 var sideB = B * B;
 var sideC = C * C;
 
 
-// Console
+// Outputs
+// code to check if the user entered a value for side A and side B
 if (A && B > 0) {
-    console.log("Side A² =" + " " + sideA);
-    console.log("Side B² =" + " " + sideB);
-    console.log("Side C² =" + " " + sideA + sideB);
+    console.log("Side A² = " + sideA);// displays the value of side A²
+    console.log("Side B² = " + sideB);// displays the value of side B²
+    console.log("Side C² = " + (sideA + sideB));// displays the value of side A² + side B²
+    console.log("The hypotenuse is " + Math.sqrt(sideA + sideB) + "²");// displays the square root of side C²
 } else if (A && C > 0){
-    console.log("Side A² =" + " " + sideA);
-    console.log("Side B² =" + " " + (sideC - sideA);
-    console.log("Side C² =" + " " + sideC);
+    console.log("Side A² = " + sideA);// displays the value of side A²
+    console.log("Side B² = " + (sideC - sideA));// displays the value of side B by taking side C² - side A²
+    console.log("Side C² = " + sideC);// displays the value of side C²
+    console.log("The hypotenuse is " + C + "²");// displays the hypotenuse the user already provided
 } else {
-    console.log("Side A² =" + " " + (sideC - sideB));
-    console.log("Side B² =" + " " + sideB);
-    console.log("Side C² =" + " " + sideC);
+    console.log("Side A² = " + (sideC - sideB));// displays the value of side A² by subtracting side B² from side C²
+    console.log("Side B² = " + sideB);// displays the value of side B²
+    console.log("Side C² = " + sideC);// displays the value of side C²
+    console.log("The hypotenuse is " + C + "²");// displays the hypotenuse the user already provided
 }
+
